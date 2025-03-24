6b3e277982f1f3488c63b4e7b1c06a9a13d899a5
fame2332 <rc202300000@gmail.com>
2025-03-25 01:03:13 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    target: 'esnext', // Ensures support for top-level await
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
